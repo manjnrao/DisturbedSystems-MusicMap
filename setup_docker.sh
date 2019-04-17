b81b5bfcf342a85d1b2129ea6e85699955244d86
ArchanaAnand0212 <43487108+ArchanaAnand0212@users.noreply.github.com>
2019-04-16 20:33:28 -0600

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,5 +1,5 @@
-sudo apt install default-jre
-sudo apt install default-jdk
+yes Y | sudo apt install default-jre
+yes Y | sudo apt install default-jdk
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
