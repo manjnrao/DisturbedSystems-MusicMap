b82d47e51a4abe3255d148415ec061857ffcfe19
manjnrao <43648893+manjnrao@users.noreply.github.com>
2019-04-20 14:04:51 -0600

    made seeds parameter mandatory

--- setup_node.sh
+++ setup_node.sh
@@ -1,7 +1,6 @@
 #first argument is node name (mandatory)
 #second argument is VM instance IP(mandatory)
-#third and forth arguments are used together and is optional
-#third argument is  -e  
-#forth argument CASSANDRA_SEEDS=<ip of all seeds comma seperated>
 
-eval sudo docker run --name $1 -d -e CASSANDRA_BROADCAST_ADDRESS=$2 -p 7000:7000 $3 $4 strapdata/elassandra
+#third argument <ip of all seeds comma seperated>(mandatory)
+
+eval sudo docker run --name $1 -d -e CASSANDRA_BROADCAST_ADDRESS=$2 -p 7000:7000 -e CASSANDRA_SEEDS=$3 strapdata/elassandra
